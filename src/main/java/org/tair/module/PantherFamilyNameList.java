d06756233b98cf6e9acc8ec1a2fd1375ddc7c0b4
Swapnil Sawant <swapnil.sawant@arabidopsis.org>
2019-01-28 14:54:04 -0800

    pipeline for downloading and indexing pruned trees

--- src/main/java/org/tair/module/PantherFamilyNameList.java
+++ src/main/java/org/tair/module/PantherFamilyNameList.java
@@ -2,14 +2,14 @@ package org.tair.module;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
-@JsonInclude(Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Parameters {
-	private String elapsed_time;
-	private String id;
-	private String book;
+public class PantherFamilyNameList {
+    private List<FamilyName> familyNames = new ArrayList<>();
 }
--- src/main/java/org/tair/process/PantherETLPipeline.java
+++ src/main/java/org/tair/process/PantherETLPipeline.java
@@ -2,32 +2,109 @@ package org.tair.process;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.slf4j.Logger;
+
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.tair.module.Annotation;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import org.slf4j.LoggerFactory;
+import org.tair.module.FamilyName;
 import org.tair.module.PantherData;
+import org.tair.module.PantherFamilyList;
+import org.tair.module.PantherFamilyNameList;
+import org.tair.util.Util;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+
 import java.util.stream.Stream;
+
 
 public class PantherETLPipeline {
 
 	String urlString = "http://localhost:8983/solr/panther";
 //	String urlString = "http://54.68.67.235:8983/solr/panther";
-	String localPantherBooksFolder = "/Users/swapp1990/Documents/projects/Pheonix_Projects/PantherPipeline/books2/";
+	String localPantherBooksFolder = "/Users/swapp1990/Documents/projects/Pheonix_Projects/pruned_panther_files/";
 	SolrClient solr = new HttpSolrClient.Builder(urlString).build();
 	ObjectMapper mapper = new ObjectMapper();
+	int committedCount = 0;
+
+	//Get Panther Family List from panther web server and convert to a json string and save locally
+	public String getPantherFamilyListFromServer() throws Exception {
+		String url = "http://panthertest1.med.usc.edu:8081/tempFamilySearch?type=family_list";
+		return Util.readContentFromWebUrlToJsonString(url);
+	}
+
+	public void savePantherFamilyListJsonLocally(String jsonString) throws Exception {
+		String filePath = "src/main/resources/panther/familyList.json";
+		File jsonFile = new File(filePath);
+		jsonFile.setExecutable(true);
+		jsonFile.setReadable(true);
+		jsonFile.setWritable(true);
+		jsonFile.createNewFile();
+		mapper.writeValue(jsonFile, jsonString);
+	}
+
+	//Get Panther Family List from local file if it exists
+	public List<String> getLocalPantherFamilyList() throws Exception {
+		String filePath = "src/main/resources/panther/familyList.json";
+		InputStream input = new FileInputStream(filePath);
+
+		String data = mapper.readValue(input, String.class);
+
+		PantherFamilyList flJson = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT).readValue(data,
+				PantherFamilyList.class);
+		return flJson.getFamilyList();
+	}
+
+	//Get Panther Family Name List from local file if it exists
+	public Map<String, String> getLocalPantherFamilyNamesList() throws Exception {
+		String filePath = "src/main/resources/panther/familyNamesList.json";
+		InputStream input = new FileInputStream(filePath);
+
+		String data = mapper.readValue(input, String.class);
+
+		PantherFamilyNameList flJson = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT).readValue(data,
+				PantherFamilyNameList.class);
+
+		Map<String, String> idToFamilyName = new HashMap<String, String>();
+		for(int i = 0; i < flJson.getFamilyNames().size(); i++) {
+			FamilyName familyNameObj = flJson.getFamilyNames().get(i);
+			idToFamilyName.put(familyNameObj.getPantherId(), familyNameObj.getFamilyName());
+		}
+		return idToFamilyName;
+	}
+
+	public void savePantherTreeLocallyById(String familyId, int idx) throws Exception {
+		PantherData pantherData = new PantherBookXmlToJson().readPantherTreeById(familyId);
+		String filePath = localPantherBooksFolder + familyId+ ".json";
+		try {
+			savePantherJsonToLocal(filePath, pantherData);
+			System.out.println("Saved: "+pantherData.getId() + " idx: " + idx);
+		} catch(Exception e) {
+			System.out.println("Error in saving "+pantherData.getId());
+		}
+	}
+
+	public void savePantherTreesLocally(List<String> familyList) throws Exception {
+		for(int i = 3500; i < familyList.size(); i++) {
+			String familyId = familyList.get(i);
+			savePantherTreeLocallyById(familyId, i);
+		}
+	}
 
 	public void readPantherBooksList() throws Exception {
 
 		StringBuilder data = new StringBuilder();
-		String fileName = "/Users/swapp1990/Documents/projects/Pheonix_Projects/PantherDataPipeline/src/main/java/org/tair/module/panther_books.html";
+		String fileName = "/Users/swapp1990/Documents/projects/Pheonix_Projects/PantherPipeline/src/main/java/org/tair/module/panther_books.html";
 		Stream<String> lines = Files.lines(Paths.get(fileName));
 		lines.forEach(line -> data.append(line).append("\n"));
 		lines.close();
@@ -58,11 +135,65 @@ public class PantherETLPipeline {
 			PantherData pantherData = new PantherBookXmlToJson().readBookFromLocal(readPantherBooksFromLocal(id));
 
 			pantherList.add(pantherData);
-			System.out.println("Stored " + id);
+//			System.out.println("Stored " + id);
 		}
 		saveAndCommitToSolr(pantherList);
 		pantherList.clear();
-		System.out.println("Success!");
+		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Success!");
+	}
+
+	public void indexSolrDB(Map<String, String> idToFamilyNames) throws Exception {
+		List<String> pantherFamilyList = getLocalPantherFamilyList();
+		List<PantherData> pantherList = new ArrayList<>();
+		List<String> emptyPantherIds = new ArrayList<>();
+		int commitCount = 500;
+		for(int i = 0; i < 5; i++) {
+			PantherData origPantherData = readPantherBooksFromLocal(pantherFamilyList.get(i));
+			String familyName = idToFamilyNames.get(pantherFamilyList.get(i));
+			PantherData modiPantherData = new PantherBookXmlToJson().convertJsonToSolrDocument(origPantherData, familyName);
+			if(modiPantherData != null) {
+				pantherList.add(modiPantherData);
+			} else {
+				emptyPantherIds.add(origPantherData.getId());
+			}
+			if(pantherList.size() >= commitCount) {
+				saveAndCommitToSolr(pantherList);
+				pantherList.clear();
+			}
+		}
+		saveAndCommitToSolr(pantherList);
+		pantherList.clear();
+	}
+
+	public void savePantherFamilyNamesLocally() throws Exception {
+		List<String> pantherFamilyList = getLocalPantherFamilyList();
+		JSONObject jo = new JSONObject();
+		Collection<JSONObject> items = new ArrayList<JSONObject>();
+		for(int i = 0; i < pantherFamilyList.size(); i++) {
+			JSONObject item = new JSONObject();
+			item.put("pantherId", pantherFamilyList.get(i));
+			item.put("familyName", readFamilyNameFromUrl(pantherFamilyList.get(i)));
+			items.add(item);
+			if(items.size() % 100 == 0) {
+				System.out.println(items.size() + " items added");
+			}
+		}
+		String filePath = "src/main/resources/panther/familyNamesList.json";
+		File jsonFile = new File(filePath);
+		jsonFile.setExecutable(true);
+		jsonFile.setReadable(true);
+		jsonFile.setWritable(true);
+		jsonFile.createNewFile();
+		jo.put("familyNames", new JSONArray(items));
+
+		mapper.writeValue(jsonFile, jo.toString());
+	}
+
+	public String readFamilyNameFromUrl(String family_id) throws Exception {
+		String flUrl = "http://panthertest1.med.usc.edu:8081/tempFamilySearch?type=family_name&book=" + family_id;
+		String jsonString = Util.readFamilyNameFromUrl(flUrl);
+		String familyName = new JSONObject(jsonString).getJSONObject("search").getString("family_name");
+		return familyName;
 	}
 
 	public PantherData readPantherBooksFromLocal(String id) throws Exception {
@@ -85,14 +216,22 @@ public class PantherETLPipeline {
 	public void saveAndCommitToSolr(List<PantherData> pantherList) throws Exception{
 		solr.addBeans(pantherList);
 		solr.commit();
-		System.out.println("Commit!");
+		committedCount += pantherList.size();
+		System.out.println("Commit! "+committedCount);
 	}
 
 	public static void main(String args[]) throws Exception {
-
 		PantherETLPipeline etl = new PantherETLPipeline();
-		etl.readPantherBooksList();
 
+//		etl.readPantherBooksList();
+//		String familyListJson = etl.getPantherFamilyListFromServer();
+//		etl.savePantherFamilyListJsonLocally(familyListJson);
+// 		etl.savePantherFamilyNamesLocally();
+//		List<String> pantherFamilyList = etl.getLocalPantherFamilyList();
+//		etl.savePantherTreesLocally(pantherFamilyList);
+
+		Map<String, String> idToFamilyNames = etl.getLocalPantherFamilyNamesList();
+		etl.indexSolrDB(idToFamilyNames);
 	}
 
 }
