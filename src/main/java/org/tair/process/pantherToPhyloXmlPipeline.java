c7873b5b9db96fb2ecb7b154e80dbfbdd1fb6a92
Swapnil Sawant <swapp19902@gmail.com>
2020-02-27 14:23:11 -0800

    Panther 15 pipeline (#14)
    * Added my code for panther json to phylo xml pipeline. Added panther and phyloxml under resources directory. Included some sample panther json files in the pruned_panther_files directory. Created separate modules for my java classes under module directory.
    
    * removed phylo xml files accidently commited in phyloxml directory so user can see the files created after running pipeline
    
    * changed some comments
    
    * panther 15 index only book_info
    
    * save tree to s3 while indexing
    
    * panther 15 release changes
    
    * panther 15 changes
    
    * update grafting and pruning tree url and taxon ids list for panther 15
    
    * cleanup
    
    * pruning and grafting url update for panther 15
    
    * merged phyloxml code and upload to s3
    
    * phyloxml download

---
+++ src/main/java/org/tair/process/pantherToPhyloXmlPipeline.java
@@ -0,0 +1,272 @@
+package org.tair.process;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.tair.module.pantherForPhylo.*;
+import org.tair.module.phyloxml.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.StreamResult;
+/*
+    Purpose:
+        Input: place Panther Json Files in main/resources/panther/pruned_panther_files directory
+        Output: receive Json files in xml format in main/resources/phyloxml directory
+
+        There are some sample panther json files in there currently.
+*/
+public class pantherToPhyloXmlPipeline
+{
+    private static String RESOURCES_BASE = "/Users/swapp1990/Documents/projects/Pheonix_Projects/phylogenes_data/PantherPipelineResources/panther15/panther";
+    PantherS3Wrapper pantherS3Server = new PantherS3Wrapper();
+
+    public static void main(String args[])
+    {
+//        PantherJsonToPhyloXml("PTHR11705");
+
+//
+//        convertAllInDirectory();   // converts all panther json files in pruned_panther_files directory
+        pantherToPhyloXmlPipeline pxml = new pantherToPhyloXmlPipeline();
+        pxml.uploadAlltoS3();
+    }
+
+    // specifically converts all pruned pantherForPhylo files from resource directory
+    // and places them into phyloxml directory
+    static void convertAllInDirectory()
+    {
+        File dir = new File(RESOURCES_BASE + "/pruned_panther_files");
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null)
+        {
+            int fileCount = 0;
+            for (File child : directoryListing)
+            {
+                if (child.getName().charAt(0) != '.') // to ignore files such as .gitignore and .ds_store
+                    fileCount++;
+                    PantherJsonToPhyloXml(child.getName().replace(".json", ""));
+                    System.out.println("Saved "+ fileCount + " " + child.getName());
+            }
+        }
+    }
+
+    void uploadAlltoS3() {
+        File dir = new File(RESOURCES_BASE + "/phyloXml");
+        String bucketName = "phyloxml-15";
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null) {
+            int fileCount = 0;
+            for (File child : directoryListing) {
+                if (child.getName().charAt(0) != '.') {// to ignore files such as .gitignore and .ds_store
+                    fileCount++;
+                    pantherS3Server.uploadObjectToBucket(bucketName, child.getName(), child);
+                    System.out.println("Saved S3: "+ fileCount + " " + child.getName());
+                }
+            }
+        }
+    }
+
+    // Converts a single pantherForPhylo Json to Phylo Xml file
+    // only input file name without file extension
+    // files r normally located in main/resources/pruned_panther_files, however
+    // u can change path if json files placed elsewhere
+    static void PantherJsonToPhyloXml(String fileName)
+    {
+        // declarations for mapping pantherForPhylo json objects to java objects
+        ObjectMapper objectMapper = new ObjectMapper();
+        Panther panther = new Panther();
+        try
+        {
+            // mapping pantherForPhylo json file to pantherj java object
+            panther = objectMapper.readValue(new File(RESOURCES_BASE + "/pruned_panther_files/"+fileName+".json"), Panther.class);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        // some objects in the pruned files are empty with just an ID, this avoids creating an xml for it
+        if (panther.getSearch() != null)
+        {
+            // creating the root node and setting up transformation(pantherForPhylo -> phyloxml)
+            Phyloxml phylo = createPhyloRoot(loadHashWithFamNames(), panther);
+            // transformation pantherForPhylo object to phyloxml object, build phyloxml tree while level order traversal
+            constructPhyloTreeWithAnnoTree(panther.getSearch().getAnnotation_node(), phylo.getPhylogeny().getClade());
+            // transform phlyo java object to xml local file
+            String xmlLocalFile_fullPath = RESOURCES_BASE + "/phyloXml/" + fileName+".xml";
+            phyloObjToXML(phylo, xmlLocalFile_fullPath);
+        }
+    }
+
+    // change path if family names placed elsewhere
+    static HashMap<String, String> loadHashWithFamNames()
+    {
+        // declarations for loading in hashmap values for family name and id key value pairs
+        ObjectMapper mapper = new ObjectMapper();
+        familyList fam = new familyList();
+        fam.setFamilyNames(new ArrayList<FamilyNames>());
+        HashMap<String, String> hm = new HashMap<String, String>();
+        try
+        {
+            // loading in hashmap values for family name and id key value pairs
+            InputStream input = new FileInputStream(RESOURCES_BASE + "/familyNamesList.json");
+            String data = mapper.readValue(input, String.class);
+            fam = mapper.readValue(data, familyList.class); //.enable(SerializationFeature.INDENT_OUTPUT)
+            for (int i = 0; i < fam.getFamilyNames().size(); i++)
+                hm.put(fam.getFamilyNames().get(i).getPantherId(), fam.getFamilyNames().get(i).getFamilyName());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return hm;
+    }
+
+//    Creates the first node, and classes containing the node before
+//    passing that root to a function that creates the tree.
+    static Phyloxml createPhyloRoot(HashMap<String, String> hm, Panther panther)
+{
+    Phyloxml phylo = new Phyloxml();
+    phylo.setPhylogeny(new Phylogeny());
+    phylo.getPhylogeny().setClade(new Clade());
+    phylo.getPhylogeny().getClade().setEvents(new Events());
+    phylo.getPhylogeny().setDescription(panther.getId());
+    phylo.getPhylogeny().setRooted("true");                     // CHECK WHEN TO KNOW TRUE AND FALSE
+    phylo.getPhylogeny().setName(hm.get(panther.getId()));
+    return phylo;
+}
+
+
+//    Sets clade attributes equal to module.pantherForPhylo.Annotation attributes but only if they are needed so XML file looks correct.
+//    So we only allocate memory for objects when we see the module.pantherForPhylo.Annotation node attributes are not empty.
+//    This is a helper function for
+    static void createAndSetCladeWithAno(Annotation anno, Clade clade)
+    {
+        if (anno.getEvent_type() != null)
+        {
+            clade.setEvents(new Events());
+            if (anno.getEvent_type().equals("SPECIATION"))
+                clade.getEvents().setSpeciations("1");
+            if (anno.getEvent_type().equals("DUPLICATION"))
+                clade.getEvents().setDuplications("1");
+        }
+        if (anno.getOrganism() != null)
+        {
+            clade.setTaxonomy(new Taxonomy());
+            clade.getTaxonomy().setScientific_name(anno.getOrganism());
+        }
+        if (anno.getNode_name() != null)
+        {
+            clade.setSequence(new Sequence());
+            clade.getSequence().setAccession(new Accession());
+            clade.getSequence().getAccession().setValue(cutAccessionString(anno.getNode_name()));
+            clade.getSequence().getAccession().setSource("UniProtKB");
+        }
+        if (anno.getBranch_length() != null)
+        {
+            clade.setBranch_length(anno.getBranch_length());
+        }
+        if (anno.getGene_id() != null)
+        {
+            clade.setName(cutGeneID(anno.getGene_id()));
+        }
+    }
+
+//    https://www.geeksforgeeks.org/generic-tree-level-order-traversal/
+//    Same algorithim as level order traversal for n-ary trees(using queue) except
+//    instead of printing when visiting the node, we set clade node attributes equal to annotation
+//    node attributes.
+    static void constructPhyloTreeWithAnnoTree(Annotation root, Clade croot)
+    {
+        if (root == null || croot == null)
+            return;
+        Queue<Annotation> anno_q = new LinkedList<Annotation>();
+        Queue<Clade> clade_q = new LinkedList<Clade>();
+        clade_q.add(croot);
+        anno_q.add(root);
+        while (!anno_q.isEmpty())
+        {
+            int n = anno_q.size();
+            while (n > 0)
+            {
+                Annotation annoTempNode = anno_q.poll();
+                Clade cladeTempNode = clade_q.poll();
+                createAndSetCladeWithAno(annoTempNode, cladeTempNode);  // This replaces printing the node
+                // without this, once you get to leaf node, you'll try to access thru a null object
+                if (annoTempNode.getChildren() != null)
+                {
+                    // in order to add n number of children, we need to allocate the child clade
+                    cladeTempNode.setClade(new LinkedList<Clade>());
+                    for (int i = 0; i < annoTempNode.getChildren().getAnnotation_node().size(); i++)
+                    {
+                        cladeTempNode.getClade().add(new Clade());    // adds n childs based on the annotation nodes amount of childs
+                        if (annoTempNode.getChildren().getAnnotation_node().get(i) != null)
+                        {
+                            anno_q.add(annoTempNode.getChildren().getAnnotation_node().get(i));
+                            // add empty clade children into queue to traverse and create next children
+                            clade_q.add(cladeTempNode.getClade().get(i));
+                        }
+                    }
+                }
+                n--;
+            }
+        }
+    }
+
+//    Given a module.phyloxml.Phyloxml object, creates a local xml file formatted to be easier to read. Change the number in
+//    the parameter to change the amount of indent space
+//    https://stackoverflow.com/questions/46708498/jaxb-marshaller-indentation fixed indent bug looking at this
+//    change path to place phyloxml elsewhere
+    private static void phyloObjToXML(Phyloxml phylo, String filePath)
+    {
+        try
+        {
+            //Create JAXB Context
+            JAXBContext jaxbContext = JAXBContext.newInstance(Phyloxml.class);
+
+            //Create Marshaller
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+            //Store XML to File
+            File file = new File(filePath);
+
+            DOMResult domResult = new DOMResult();
+            //Writes XML file to file-system
+            jaxbMarshaller.marshal(phylo, domResult);
+
+            //Required formatting
+            Transformer transformer = null;
+            transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            transformer.transform(new DOMSource(domResult.getNode()), new StreamResult(file));
+
+        }
+        catch (JAXBException e)
+        {
+            e.printStackTrace();
+        } catch (TransformerException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+//    Extracts the Q9UTA6 out of SCHPO|PomBase=SPAC25B8.12c|UniProtKB=Q9UTA6
+    private static String cutAccessionString(String x)
+    {
+        String[] y = x.split("UniProtKB=");
+        return (y[1]);
+    }
+
+    private static String cutGeneID(String id)
+    {
+        String[] x = id.split(":");
+        return (x[1]);
+    }
+}
