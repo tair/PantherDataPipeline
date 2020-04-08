3487ad77eafeede4b1ab2529a62aafffa8fc2d93
richardli515 <richardlee515@gmail.com>
2020-04-07 17:05:47 -0700

    PHG-245 load by file (#15)
    * PHG-245 load by file
    
    * PHG-245 automation and optimization
    
    Co-authored-by: Qian Li <qianli1987@arabidopsis.org>

--- src/main/java/org/tair/process/uniprotdb/GOAnnotationETLPipeline.java
+++ src/main/java/org/tair/process/uniprotdb/GOAnnotationETLPipeline.java
@@ -4,67 +4,141 @@
 
 package org.tair.process.uniprotdb;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.FacetField.Count;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.tair.module.GOAnnotation;
 import org.tair.module.GOAnnotationData;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class GOAnnotationETLPipeline {
 
-	String solrUrl = "http://localhost:8983/solr";
-	SolrClient solrClient = new HttpSolrClient.Builder(solrUrl).build();
-	ObjectMapper mapper = new ObjectMapper();
-	GOAnnotationUrlToJson GOAnnotationUrlToJson = new GOAnnotationUrlToJson();
+    private String solrUrl = "http://localhost:8983/solr";
+    private SolrClient solrClient = new HttpSolrClient.Builder(solrUrl).build();
+    private GOAnnotationUrlToJson GOAnnotationUrlToJson = new GOAnnotationUrlToJson();
+    private static String GO_IBA_RESOURCES_DIR = "src/main/resources/panther/GO_IBA_annotations";
+    private static String GO_IBA_LOGS_DIR = "src/main/logs/uniprot_db";
 
-	public void storeGOAnnotationToUniprotDb() throws Exception {
-		
-		final SolrQuery query = new SolrQuery("*:*");
-		query.setRows(0);
-		query.setFacet(true);
-		query.addFacetField("uniprot_ids");
-		query.setFacetLimit(-1); // -1 means unlimited
-		
-		final QueryResponse response = solrClient.query("panther", query);
-		final FacetField uniprot_ids = response.getFacetField("uniprot_ids");
-		List<Count> counts = uniprot_ids.getValues();
-		System.out.println("Total number to load: " + counts.size());
+    public void storeGOAnnotationFromApiToUniprotDb() throws Exception {
+        // remove all data from this collection
+        solrClient.deleteByQuery("uniprot_db", "*:*");
 
-		int iter = counts.size()/500+1;
-		for (int i = 0; i<iter; i++) {
-			List<String> uniprotIdList = new ArrayList<String>();
-			for (int j = i * 500; j < (i + 1) * 500; j++) {
-				if (j == counts.size())
-					break;
-				uniprotIdList.add(counts.get(j).getName());
-			}
-			System.out.println("Loading: " + i*500 +"-" +(i+1)*500);
-			List<GOAnnotationData> goAnnotations = GOAnnotationUrlToJson.readGOAnnotationUrlToObjectList(String.join(",", uniprotIdList));
-			if (goAnnotations.size() >0) {
-				solrClient.addBeans("uniprot_db",goAnnotations);
-		        solrClient.commit("uniprot_db");
-			}
-		}
-		System.out.println("finished commits to uniprot_db collection");
+        final SolrQuery query = new SolrQuery("*:*");
+        query.setRows(0);
+        query.setFacet(true);
+        query.addFacetField("uniprot_ids");
+        query.setFacetLimit(-1); // -1 means unlimited
 
-	}
+        final QueryResponse response = solrClient.query("panther", query);
+        final FacetField uniprot_ids = response.getFacetField("uniprot_ids");
+        List<Count> counts = uniprot_ids.getValues();
+        System.out.println("Total number to load: " + counts.size());
 
-	public static void main(String args[]) throws Exception {
-		long startTime = System.nanoTime();
+        int iter = counts.size() / 500 + 1;
+        for (int i = 0; i < iter; i++) {
+            List<String> uniprotIdList = new ArrayList<String>();
+            for (int j = i * 500; j < (i + 1) * 500; j++) {
+                if (j == counts.size())
+                    break;
+                uniprotIdList.add(counts.get(j).getName());
+            }
+            System.out.println("Loading: " + i * 500 + "-" + (i + 1) * 500);
+            List<GOAnnotationData> goAnnotations = GOAnnotationUrlToJson.readGOAnnotationUrlToObjectList(String.join(",", uniprotIdList));
+            if (goAnnotations.size() > 0) {
+                solrClient.addBeans("uniprot_db", goAnnotations);
+                solrClient.commit("uniprot_db");
+            }
+        }
+        System.out.println("finished commits to uniprot_db collection");
 
-		new GOAnnotationETLPipeline().storeGOAnnotationToUniprotDb();
-		
-		long endTime = System.nanoTime();		
-		long timeElapsed = endTime - startTime;
-		System.out.println("Execution time in nanoseconds  : " + timeElapsed);
-		System.out.println("Execution time in milliseconds : " + 
-								timeElapsed / 1000000);
-	}
+    }
+
+    /***
+     * Load go annotations from gaf file into solr's uniprot_db collection by the following steps:
+     * 1. prepare for resources including gaf file, obo file, properties file
+     * 2. traverse all gaf files and read go annotation data
+     * 3. commit go annotation data in batch to uniprot_db collection
+     ***/
+    public void updateGOAnnotationFromFileToUniprotDb() throws Exception {
+        // prepare for resources
+        FileUtils.cleanDirectory(new File(GO_IBA_RESOURCES_DIR));
+        GOAnnotationGafUtils goAnnotationGafUtils = new GOAnnotationGafUtils();
+        goAnnotationGafUtils.loadGoIbaAnnotationsResources(GO_IBA_RESOURCES_DIR);
+        goAnnotationGafUtils.generatePropFromObo(GO_IBA_RESOURCES_DIR);
+
+        try (
+                BufferedWriter writer = new BufferedWriter(new FileWriter(GO_IBA_LOGS_DIR + "/errorsLoadingGoAnnotationsFromFile.log"));
+                InputStream input = new FileInputStream(GO_IBA_RESOURCES_DIR + "/goidname.properties");
+        ) {
+            // load props for goName field
+            Properties prop = new Properties();
+            prop.load(input);
+
+            // get all gaf files in folder
+            File[] files = new File(GO_IBA_RESOURCES_DIR).listFiles(file -> file.toString().endsWith(".gaf"));
+            for (File file : files) {
+                System.out.println("Processing: " + file);
+                try (
+                        FileReader fileReader = new FileReader(file);
+                        BufferedReader br = new BufferedReader(fileReader);
+                ) {
+                    String thisLine = null;
+                    List<GOAnnotationData> goAnnotations = new ArrayList<>();
+                    int lineNum = 1;
+                    while ((thisLine = br.readLine()) != null) {
+                        // validate line
+                        String validateMsg = goAnnotationGafUtils.validateGafLine(thisLine);
+                        if (!validateMsg.equals("Valid")) {
+                            writer.write(validateMsg + "\nline " + lineNum++ + ", file " + file.getName() + "\nline content: " + thisLine + "\n");
+                            continue;
+                        }
+                        // read from line and create GOAnnotation obj
+                        GOAnnotation goAnnotation = GOAnnotation.readFromGafLine(thisLine, prop);
+                        // create GOAnnotationData obj from GOAnnotation obj
+                        GOAnnotationData goAnnotationData = GOAnnotationData.createFromGOAnnotation(goAnnotation);
+
+                        goAnnotations.add(goAnnotationData);
+//                        System.out.println(goAnnotationData.toString());
+                        if (goAnnotations.size() >= 500) {
+                            System.out.println("processed: " + lineNum);
+                            solrClient.addBeans("uniprot_db", goAnnotations);
+                            solrClient.commit("uniprot_db");
+                            goAnnotations = new ArrayList<>();
+                        }
+                        lineNum++;
+                    }
+                    // commit remaining annotations
+                    solrClient.addBeans("uniprot_db", goAnnotations);
+                    solrClient.commit("uniprot_db");
+                    System.out.println("processed: " + lineNum);
+                }
+            }
+        }
+        System.out.println("finished adding data to uniprot_db collection from gaf files");
+    }
+
+    public static void main(String args[]) throws Exception {
+        long startTime = System.nanoTime();
+
+        GOAnnotationETLPipeline goAnnotationETLPipeline = new GOAnnotationETLPipeline();
+
+        // uncomment based on your needs
+        // important: if the url of gaf file or obo file changes, we need to update them in applications.properties file, otherwise it may not reflect the correct data;
+        // if the format of gaf file or obo file has been changed, we need to change the code accordingly.
+//        goAnnotationETLPipeline.storeGOAnnotationFromApiToUniprotDb();
+//        goAnnotationETLPipeline.updateGOAnnotationFromFileToUniprotDb();
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time in nanoseconds  : " + timeElapsed);
+        System.out.println("Execution time in milliseconds : " +
+                timeElapsed / 1000000);
+    }
 }
