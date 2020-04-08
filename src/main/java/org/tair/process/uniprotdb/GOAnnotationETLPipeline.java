1f7991b4e5ecbf6f11956e6e3ca08d5bbcf344ed
Qian Li <qianli1987@arabidopsis.org>
2020-04-07 17:01:36 -0700

    PHG-245 automation and optimization

--- src/main/java/org/tair/process/uniprotdb/GOAnnotationETLPipeline.java
+++ src/main/java/org/tair/process/uniprotdb/GOAnnotationETLPipeline.java
@@ -4,21 +4,18 @@
 
 package org.tair.process.uniprotdb;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
+import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.FacetField.Count;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.tair.module.ConnectedXref;
 import org.tair.module.GOAnnotation;
 import org.tair.module.GOAnnotationData;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -26,11 +23,13 @@ public class GOAnnotationETLPipeline {
 
     private String solrUrl = "http://localhost:8983/solr";
     private SolrClient solrClient = new HttpSolrClient.Builder(solrUrl).build();
-    private ObjectMapper mapper = new ObjectMapper();
-    private ObjectWriter ow = new ObjectMapper().writer();
     private GOAnnotationUrlToJson GOAnnotationUrlToJson = new GOAnnotationUrlToJson();
+    private static String GO_IBA_RESOURCES_DIR = "src/main/resources/panther/GO_IBA_annotations";
+    private static String GO_IBA_LOGS_DIR = "src/main/logs/uniprot_db";
 
     public void storeGOAnnotationFromApiToUniprotDb() throws Exception {
+        // remove all data from this collection
+        solrClient.deleteByQuery("uniprot_db", "*:*");
 
         final SolrQuery query = new SolrQuery("*:*");
         query.setRows(0);
@@ -62,96 +61,68 @@ public class GOAnnotationETLPipeline {
 
     }
 
-    public void storeGOAnnotationFromFileToUniprotDb() throws Exception {
+    /***
+     * Load go annotations from gaf file into solr's uniprot_db collection by the following steps:
+     * 1. prepare for resources including gaf file, obo file, properties file
+     * 2. traverse all gaf files and read go annotation data
+     * 3. commit go annotation data in batch to uniprot_db collection
+     ***/
+    public void updateGOAnnotationFromFileToUniprotDb() throws Exception {
+        // prepare for resources
+        FileUtils.cleanDirectory(new File(GO_IBA_RESOURCES_DIR));
+        GOAnnotationGafUtils goAnnotationGafUtils = new GOAnnotationGafUtils();
+        goAnnotationGafUtils.loadGoIbaAnnotationsResources(GO_IBA_RESOURCES_DIR);
+        goAnnotationGafUtils.generatePropFromObo(GO_IBA_RESOURCES_DIR);
+
         try (
-            BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/panther/GO_IBA_annotations/errorsLoadingGoAnnotationsFromFile.txt"));
-            InputStream input = new FileInputStream("src/main/resources/panther/GO_IBA_annotations/goidname.properties");
+                BufferedWriter writer = new BufferedWriter(new FileWriter(GO_IBA_LOGS_DIR + "/errorsLoadingGoAnnotationsFromFile.log"));
+                InputStream input = new FileInputStream(GO_IBA_RESOURCES_DIR + "/goidname.properties");
         ) {
             // load props for goName field
             Properties prop = new Properties();
             prop.load(input);
 
             // get all gaf files in folder
-            File[] files = new File("src/main/resources/panther/GO_IBA_annotations/").listFiles(file -> file.toString().endsWith(".gaf"));
+            File[] files = new File(GO_IBA_RESOURCES_DIR).listFiles(file -> file.toString().endsWith(".gaf"));
             for (File file : files) {
                 System.out.println("Processing: " + file);
-                try(
-                    FileReader fileReader = new FileReader(file);
-                    BufferedReader br = new BufferedReader(fileReader);
-                ){
+                try (
+                        FileReader fileReader = new FileReader(file);
+                        BufferedReader br = new BufferedReader(fileReader);
+                ) {
                     String thisLine = null;
-                    // skip first 4 lines
-                    for (int i = 0; i < 4; i++) {
-                        br.readLine();
-                    }
-
                     List<GOAnnotationData> goAnnotations = new ArrayList<>();
-                    int line = 5;
+                    int lineNum = 1;
                     while ((thisLine = br.readLine()) != null) {
-                        //			System.out.println("processing line: " + line);
-                        String[] attributes = thisLine.split("\t");
-                        // filters
-                        if (attributes[3].equals("NOT")) {
-                            writer.write("Filtered qualifier.\nline " + line++ + ", file " + file.getName() + "\nline content: " + Arrays.toString(attributes) + "\n");
+                        // validate line
+                        String validateMsg = goAnnotationGafUtils.validateGafLine(thisLine);
+                        if (!validateMsg.equals("Valid")) {
+                            writer.write(validateMsg + "\nline " + lineNum++ + ", file " + file.getName() + "\nline content: " + thisLine + "\n");
                             continue;
                         }
-                        if (!attributes[8].equals("F") && !attributes[8].equals("P")) {
-                            writer.write("Filtered GO ascpect.\nline " + line++ + ", file " + file.getName() + "\nline content: " + Arrays.toString(attributes) + "\n");
-                            continue;
-                        }
-                        // validators
-                        if (!attributes[4].startsWith("GO:")) {
-                            writer.write("Irregular GO id.\nline " + line++ + ", file " + file.getName() + "\nline content: " + Arrays.toString(attributes) + "\n");
-                            continue;
-                        }
-                        if (!attributes[6].equals("IBA")) {
-                            writer.write("Irregular evidence code.\nline " + line++ + ", file " + file.getName() + "\nline content: " + Arrays.toString(attributes) + "\n");
-                            continue;
-                        }
-                        if (!attributes[10].startsWith("UniProtKB")) {
-                            writer.write("Irregular uniprot id.\nline " + line++ + ", file " + file.getName() + "\nline content: " + Arrays.toString(attributes) + "\n");
-                            continue;
-                        }
+                        // read from line and create GOAnnotation obj
+                        GOAnnotation goAnnotation = GOAnnotation.readFromGafLine(thisLine, prop);
+                        // create GOAnnotationData obj from GOAnnotation obj
+                        GOAnnotationData goAnnotationData = GOAnnotationData.createFromGOAnnotation(goAnnotation);
 
-                        GOAnnotation goAnnotation = new GOAnnotation();
-                        String geneProductId = attributes[10].substring(10).split("\\|")[0].toLowerCase();
-                        goAnnotation.setGeneProductId(geneProductId);
-                        String goId = attributes[4];
-                        goAnnotation.setGoId(goId);
-                        String key = goId.split(":")[1];
-                        goAnnotation.setGoName(prop.getProperty(key));
-                        goAnnotation.setReference(attributes[5]);
-                        goAnnotation.setEvidenceCode("ECO:0000318," + attributes[6] + ",phylogeny");
-                        String withFrom = attributes[7];
-                        List<ConnectedXref> connectedXrefs = new ArrayList<>();
-                        for (String entry : withFrom.split("\\|")) {
-                            String db = entry.split(":")[0];
-                            String id = entry.substring(db.length() + 1);
-                            ConnectedXref connectedXref = new ConnectedXref();
-                            connectedXref.setDb(db);
-                            connectedXref.setId(id);
-                            connectedXrefs.add(connectedXref);
-                        }
-                        goAnnotation.setWithFrom(connectedXrefs);
-
-                        GOAnnotationData goAnnotationData = new GOAnnotationData();
-                        goAnnotationData.setUniprot_id(geneProductId);
-                        goAnnotationData.setGo_annotations(ow.writeValueAsString(goAnnotation));
                         goAnnotations.add(goAnnotationData);
-                        System.out.println(goAnnotationData.toString());
-                            if (goAnnotations.size() >=500) {
-                                solrClient.addBeans("uniprot_db",goAnnotations);
-                                solrClient.commit("uniprot_db");
-                                goAnnotations = new ArrayList<>();
-                            }
-                        line++;
+//                        System.out.println(goAnnotationData.toString());
+                        if (goAnnotations.size() >= 500) {
+                            System.out.println("processed: " + lineNum);
+                            solrClient.addBeans("uniprot_db", goAnnotations);
+                            solrClient.commit("uniprot_db");
+                            goAnnotations = new ArrayList<>();
+                        }
+                        lineNum++;
                     }
                     // commit remaining annotations
-                    solrClient.addBeans("uniprot_db",goAnnotations);
+                    solrClient.addBeans("uniprot_db", goAnnotations);
                     solrClient.commit("uniprot_db");
+                    System.out.println("processed: " + lineNum);
                 }
             }
         }
+        System.out.println("finished adding data to uniprot_db collection from gaf files");
     }
 
     public static void main(String args[]) throws Exception {
@@ -160,9 +131,10 @@ public class GOAnnotationETLPipeline {
         GOAnnotationETLPipeline goAnnotationETLPipeline = new GOAnnotationETLPipeline();
 
         // uncomment based on your needs
+        // important: if the url of gaf file or obo file changes, we need to update them in applications.properties file, otherwise it may not reflect the correct data;
+        // if the format of gaf file or obo file has been changed, we need to change the code accordingly.
 //        goAnnotationETLPipeline.storeGOAnnotationFromApiToUniprotDb();
-//        goAnnotationETLPipeline.storeGOAnnotationFromFileToUniprotDb();
-
+//        goAnnotationETLPipeline.updateGOAnnotationFromFileToUniprotDb();
         long endTime = System.nanoTime();
         long timeElapsed = endTime - startTime;
         System.out.println("Execution time in nanoseconds  : " + timeElapsed);
