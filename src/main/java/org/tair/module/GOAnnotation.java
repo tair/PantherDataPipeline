181354ed05ff680a12479d077b211e813cd70811
richardli515 <richardlee515@gmail.com>
2020-04-07 17:05:47 -0700

    PHG-245 load by file (#15)
    * PHG-245 load by file
    
    * PHG-245 automation and optimization
    
    Co-authored-by: Qian Li <qianli1987@arabidopsis.org>

--- src/main/java/org/tair/module/GOAnnotation.java
+++ src/main/java/org/tair/module/GOAnnotation.java
@@ -1,89 +1,111 @@
 package org.tair.module;
 
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 @Data
 @JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GOAnnotation {
-	private String geneProductId;
-	private String goId;
-	private String goName;
-	private String evidenceCode;
-	private String reference;
-	private List<ConnectedXref> withFrom;
-	
-	@JsonProperty("geneProductId")
-	private void buildGeneProductId(String geneProductId) {
-		if (geneProductId.startsWith("UniProtKB:")) {
-			this.geneProductId = geneProductId.split(":")[1];
-		} else {
-			this.geneProductId = geneProductId;
-			try {
-			    BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/irregularGeneProductId.txt", true));
-			    writer.write(geneProductId+"\n");
-			    writer.close();
-			}catch (IOException ex) {
-	    		ex.printStackTrace();
-	    	}
-		}
-	}
-	
+    private String geneProductId;
+    private String goId;
+    private String goName;
+    private String evidenceCode;
+    private String reference;
+    private List<ConnectedXref> withFrom;
+
+    @JsonProperty("geneProductId")
+    private void buildGeneProductId(String geneProductId) {
+        if (geneProductId.startsWith("UniProtKB:")) {
+            this.geneProductId = geneProductId.split(":")[1];
+        } else {
+            this.geneProductId = geneProductId;
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/irregularGeneProductId.txt", true));
+                writer.write(geneProductId + "\n");
+                writer.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
     @JsonProperty("withFrom")
     private void buildWithFrom(List<WithFromItem> withFrom) {
-    	if (withFrom != null) {    		    	
-			List<ConnectedXref> withFromList = new ArrayList<ConnectedXref>();
-			for (WithFromItem withFromItem: withFrom) {
-				List<ConnectedXref> connectedXrefs =  withFromItem.getConnectedXrefs();
-				for (ConnectedXref connectedXref:connectedXrefs) {
-					withFromList.add(connectedXref);
-				}
-			}
-	        this.withFrom = withFromList;
-    	}else {
-    		this.withFrom = null;
-    	}
+        if (withFrom != null) {
+            List<ConnectedXref> withFromList = new ArrayList<ConnectedXref>();
+            for (WithFromItem withFromItem : withFrom) {
+                List<ConnectedXref> connectedXrefs = withFromItem.getConnectedXrefs();
+                for (ConnectedXref connectedXref : connectedXrefs) {
+                    withFromList.add(connectedXref);
+                }
+            }
+            this.withFrom = withFromList;
+        } else {
+            this.withFrom = null;
+        }
     }
-    
+
     @JsonProperty("evidenceCode")
     private void buildEvidence(String evidenceCode) {
-    	Properties prop = new Properties();
-    	InputStream input = null;
+        Properties prop = new Properties();
+        InputStream input = null;
 
-    	try {
+        try {
 
-    		input = new FileInputStream("src/main/resources/evidence.properties");
+            input = new FileInputStream("src/main/resources/evidence.properties");
 
-    		// load a properties file
-    		prop.load(input);
-    		
-    		String key = evidenceCode.split(":")[1];
-    		this.evidenceCode = prop.getProperty(key);
-    		
-    	} catch (IOException ex) {
-    		ex.printStackTrace();
-    	} finally {
-    		if (input != null) {
-    			try {
-    				input.close();
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    		}
-    	}
+            // load a properties file
+            prop.load(input);
+
+            String key = evidenceCode.split(":")[1];
+            this.evidenceCode = prop.getProperty(key);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // read data from a line of gaf file and return a GOAnnotation obj
+    public static GOAnnotation readFromGafLine(String lineStr, Properties prop) {
+        String[] attributes = lineStr.split("\t");
+        GOAnnotation goAnnotation = new GOAnnotation();
+        String geneProductId = attributes[10].substring(10).split("\\|")[0].toLowerCase();
+        goAnnotation.setGeneProductId(geneProductId);
+        String goId = attributes[4];
+        goAnnotation.setGoId(goId);
+        String key = goId.split(":")[1];
+        goAnnotation.setGoName(prop.getProperty(key)); // get go name by go id from prop file
+        goAnnotation.setReference(attributes[5]);
+        // compose evidence code string for IBA, if in the future there are more evidence codes, this should be changed accordingly
+        goAnnotation.setEvidenceCode("ECO:0000318," + attributes[6] + ",phylogeny");
+        String withFrom = attributes[7];
+        List<ConnectedXref> connectedXrefs = new ArrayList<>();
+        for (String entry : withFrom.split("\\|")) {
+            String db = entry.split(":")[0];
+            String id = entry.substring(db.length() + 1);
+            ConnectedXref connectedXref = new ConnectedXref();
+            connectedXref.setDb(db);
+            connectedXref.setId(id);
+            connectedXrefs.add(connectedXref);
+        }
+        goAnnotation.setWithFrom(connectedXrefs);
+        return goAnnotation;
     }
 }
---
+++ src/main/java/org/tair/process/uniprotdb/README.md
@@ -0,0 +1,32 @@
+# ETL Pipeline for GO Annotation Data
+
+- GOAnnotationETLPipeline.java file is used to index the uniprot_db collection of the solr database.
+- GOAnnotationUrlToJson.java provides helper methods to read data from url.
+- UpdateGOAnnotations.java is used to update go_annotations field in panther collection with the data in uniprot_db.
+
+## Methods to Load Data
+1. Load GO Annotation Data from API Url
+We use method storeGOAnnotationFromApiToUniprotDb() in GOAnnotationETLPipeline.java to load GO Annotation data from QuickGO API url.
+In this method, the following process will happen:
+- Delete all current data of uniprot_db collection.
+- Read xml file from QuickGO API(<https://www.ebi.ac.uk/QuickGO/>) and convert it to JSON format.
+- Parse the json string and store data in GOAnnotation object.
+- Commit GOAnnotation object to uniprot_db collection of solr database.
+
+2. Add GO Annotation Data from Static File
+We use method updateGOAnnotationFromFileToUniprotDb() in GOAnnotationETLPipeline.java to add GO Annotation data from static file on top of existing data.
+In this method, the following process will happen:
+- Download and unzip all gaf files from <ftp://ftp.pantherdb.org/downloads/paint/presubmission>.
+- Download obo file from <http://current.geneontology.org/ontology/go-basic.obo> and convert obo file to properties file.
+- Read and parse all gaf files and get the attributes of an annotation per line.
+- Commit GOAnnotation object to uniprot_db collection of solr database.
+
+** important: if the url of gaf file or obo file changes, we need to update them in applications.properties file, otherwise it may not reflect the correct data; if the format of gaf file or obo file has been changed, we need to change the code accordingly. **
+
+The above 2 methods are called from GOAnnotationETLPipelilne.java's main method.
+
+## Steps to Run:
+- 1. run GOAnnotationETLPipelilne.java
+- 2. run UpdateGOAnnotations.java
+
+We can also invoke them in PantherETLPipeline.java's main method which will be more convenient for the full reindex.
