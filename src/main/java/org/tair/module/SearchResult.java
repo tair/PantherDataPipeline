7cedbeeceb4772ad318f39df462dce18bd7568e1
Swapnil Sawant <swapnil.sawant@arabidopsis.org>
2020-06-08 17:54:29 -0700

    hotfix: pruning grafting url change

--- src/main/java/org/tair/module/SearchResult.java
+++ src/main/java/org/tair/module/SearchResult.java
@@ -1,11 +1,13 @@
 package org.tair.module;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Data;
 
 @Data
 @JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchResult {
 	private Parameters parameters;
 	private Annotation annotation_node;
