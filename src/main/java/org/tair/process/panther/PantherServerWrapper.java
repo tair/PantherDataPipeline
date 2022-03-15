11cf644ae1111457b1190100e54a3ae3ef600c42
Qian Li <richardlee515@gmail.com>
2022-03-15 16:37:11 -0700

    TAIR-3505 tair phylogenes section

--- src/main/java/org/tair/process/panther/PantherServerWrapper.java
+++ src/main/java/org/tair/process/panther/PantherServerWrapper.java
@@ -2,6 +2,7 @@ package org.tair.process.panther;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import javafx.util.Pair;
 import org.json.JSONObject;
 import org.tair.module.PantherData;
 import org.tair.module.ortho.OrthoMapped;
@@ -11,7 +12,9 @@ import org.tair.module.paralog.Mapped;
 import org.tair.module.paralog.ParalogMapping;
 import org.tair.util.Util;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -102,7 +105,7 @@ public class PantherServerWrapper {
         return Util.readJsonFromUrl(msaTreeUrl);
     }
 
-    public String callOrtholog_uniprot(String uniprotId, Map<String, String> uniprot2agi_mapping) throws Exception {
+    public List<String> callOrtholog_uniprot(String uniprotId, Map<String, String> uniprot2agi_mapping, Map<String, List<String>> organisms_mapping) throws Exception {
         String url = PANTHER_ORTHO_URL +"/matchortho?geneInputList="+ uniprotId
                 + "&organism=3702&targetOrganism=" +
                 "13333%2C15368%2C51351%2C3055%2C2711%2C3659%2C4155%2C3847%2C3635%2C4232%2C112509%2C3880%2C214687%2C4097%2C39947%2C70448%2C42345%2C3218%2C3694%2C3760%2C3988%2C4555%2C4081%2C4558%2C3641%2C4565%2C29760%2C4577%2C29655%2C3708%2C4072%2C71139%2C51240%2C4236%2C3983%2C4432%2C88036%2C4113%2C3562"
@@ -116,45 +119,154 @@ public class PantherServerWrapper {
 //            System.out.println(orthologs.get(i).getTarget_gene_symbol());
             OrthoMapped mapped = orthologs.get(i);
             String target_gene = mapped.getTarget_gene();
-            String uniprot_id = target_gene.split("UniProtKB=")[1];
-            mapped.setUniprot_id(uniprot_id);
+            if (!target_gene.contains("UniProtKB=")){
+                mapped.setUniprot_id("error");
+            }else{
+                String uniprot_id = target_gene.split("UniProtKB=")[1];
+                mapped.setUniprot_id(uniprot_id);
+            }
             String organism = target_gene.split("\\|")[0];
 //            System.out.println(organism);
             mapped.setOrganism(organism);
-            String gene_id = target_gene.split("\\|")[1];
+            String gene_id = target_gene.split("\\|")[1].split("=")[1];
 //            System.out.println(gene_id);
             mapped.setTarget_gene_id(gene_id);
+            List<String> organism_names = organisms_mapping.get(organism);
+//            System.out.println(organism_names);
+            String full_name = organism_names.get(0);
+            String common_name = organism_names.get(1);
+            String group_name = organism_names.get(2);
+
+            mapped.setFull_name(full_name);
+            mapped.setCommon_name(common_name);
+            mapped.setGroup_name(group_name);
+        }
+        orthologs.sort(new OrthoComparator());
+//        System.out.println(orthologs.toString());
+        StringBuilder orthoTxtSb = new StringBuilder(String.join("\t", "Organism", "UniProt ID", "Gene ID", "Ortholog Type\n"));
+        for(int i=0; i<orthologs.size(); i++) {
+            OrthoMapped mapped = orthologs.get(i);
+            String full_organism = mapped.getFull_name();
+            String common_name = mapped.getCommon_name();
+            if ( common_name != null && !common_name.equals("")){
+                full_organism += " (" + common_name + ")";
+            }
+            orthoTxtSb.append(String.join("\t", full_organism, mapped.getUniprot_id(), mapped.getTarget_gene_id(), mapped.getOrtholog() + "\n"));
         }
         ObjectMapper mapper = new ObjectMapper();
         String processedOrthologsJsonStr = mapper.writeValueAsString(orthologs);
-        return processedOrthologsJsonStr;
+//        System.out.println("processedOrthologsJsonStr"+processedOrthologsJsonStr);
+//        System.out.println("orthoTxtSb.toString() "+orthoTxtSb.toString());
+        return Arrays.asList(processedOrthologsJsonStr, orthoTxtSb.toString());
     }
 
-    public String callParalog_uniprot(String uniprotId, Map<String, String> uniprot2agi_mapping) throws Exception {
+    public class OrthoComparator implements Comparator<OrthoMapped> {
+        @Override
+        public int compare(OrthoMapped o1, OrthoMapped o2) {
+//            String organism1 = o1.getOrganism();
+//            String organism2 = o2.getOrganism();
+            if (o1.getFull_name().compareToIgnoreCase(o2.getFull_name()) != 0){
+                return o1.getFull_name().compareToIgnoreCase(o2.getFull_name());
+            } else{
+                return o1.getTarget_gene_id().compareToIgnoreCase(o2.getTarget_gene_id());
+            }
+        }
+    }
+
+    public List<String> callParalog_uniprot(String uniprotId, Map<String, String> uniprot2agi_mapping, Map<String, String> agi2symbol_mapping) throws Exception {
         String url = PANTHER_ORTHO_URL + "/homologOther?geneInputList="+uniprotId+"&organism=3702&homologType=P";
         String jsonString = Util.readJsonFromUrl(url);
+        int agiNullCount = 0;
+        int primarySymbolNullCount = 0;
+//        System.out.println("from paralog url: " + jsonString);
         try {
             ParalogMapping paraResult = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT).readValue(jsonString,
                     ParalogMapping.class);
             List<Mapped> paralogs = paraResult.getAllMappedParalogs();
+            List<Mapped> toAdd = new ArrayList<>();
+            List<Mapped> toDelete = new ArrayList<>();
+            List<String> agiNullUniprotIds = new ArrayList<>();
             for(int i=0; i<paralogs.size(); i++) {
                 Mapped para_obj = paralogs.get(i);
                 String target_gene = para_obj.getTarget_gene();
+                if (!target_gene.contains("UniProtKB=")){
+                    para_obj.setTarget_uniprot("error");
+                    para_obj.setTarget_agi("error");
+                    para_obj.setPrimary_symbol("error");
+                    continue;
+                }
                 String uniprot_id = target_gene.split("UniProtKB=")[1];
-                String agi_id = uniprot2agi_mapping.get(uniprot_id);
-//                System.out.println(uniprot_id + "->" + agi_id);
                 para_obj.setTarget_uniprot(uniprot_id);
-                para_obj.setTarget_agi(agi_id);
+                String agi_id = uniprot2agi_mapping.get(uniprot_id);
+                if ( agi_id != null && agi_id.contains("/")) {
+                    String[] agi_id_list = agi_id.split("/");
+                    System.out.println("multiple agi_ids: ");
+                    for (String agi_id_item : agi_id_list ){
+                        System.out.println(agi_id_item);
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        Mapped para_obj_temp = objectMapper.readValue(objectMapper.writeValueAsString(para_obj), Mapped.class);
+                        para_obj_temp.setTarget_agi(agi_id_item);
+                        String primarySymbol = agi2symbol_mapping.get(agi_id_item);
+                        para_obj_temp.setPrimary_symbol(primarySymbol);
+                        System.out.println(primarySymbol);
+                        toAdd.add(para_obj_temp);
+                        toDelete.add(para_obj);
+                    }
+                } else if (agi_id !=null){
+                    para_obj.setTarget_agi(agi_id);
+                    String primarySymbol = agi2symbol_mapping.get(agi_id);
+                    para_obj.setPrimary_symbol(primarySymbol);
+                } else{
+                    agiNullUniprotIds.add(uniprot_id);
+                    para_obj.setTarget_agi(null);
+                    para_obj.setPrimary_symbol(null);
+                }
+//                System.out.println(uniprot_id + "->" + agi_id);
+//                System.out.println(agi_id + " " + primarySymbol);
+            }
+            paralogs.removeAll(toDelete);
+            paralogs.addAll(toAdd);
+            paralogs.sort(new ParaComparator());
+            StringBuilder paraTxtSb = new StringBuilder(String.join("\t", "AGI ID", "Locus primary symbol\n"));
+            for(int i=0; i<paralogs.size(); i++) {
+                Mapped para_obj = paralogs.get(i);
+                if (para_obj.getTarget_agi() == null){
+                    agiNullCount += 1;
+                } else {
+                    String upperTarget_agi = para_obj.getTarget_agi().toUpperCase(Locale.ROOT);
+                    para_obj.setTarget_agi(upperTarget_agi);
+                }
+                if (para_obj.getPrimary_symbol() == null){
+                    primarySymbolNullCount += 1;
+                }
+                paraTxtSb.append(String.join("\t", para_obj.getTarget_agi(), para_obj.getPrimary_symbol() + "\n"));
+
             }
 
             ObjectMapper mapper = new ObjectMapper();
             String processedParalogsJsonStr = mapper.writeValueAsString(paralogs);
-//            System.out.println(processedParalogsJsonStr);
-            return processedParalogsJsonStr;
+            return Arrays.asList(
+                    processedParalogsJsonStr,
+                    paraTxtSb.toString(),
+                    Integer.toString(agiNullCount),
+                    Integer.toString(primarySymbolNullCount),
+                    Integer.toString(paralogs.size()),
+                    String.join(",",agiNullUniprotIds)
+            );
         }
         catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
+            System.out.println(String.join(" ","error downloading paralogs:", uniprotId, uniprot2agi_mapping.get(uniprotId),jsonString));
             return null;
+        }
+    }
+
+    public class ParaComparator implements Comparator<Mapped> {
+        @Override
+        public int compare(Mapped m1, Mapped m2) {
+            String target_agi1 = m1.getTarget_agi()!=null?m1.getTarget_agi():"null";
+            String target_agi2 = m2.getTarget_agi()!=null?m2.getTarget_agi():"null";
+            return target_agi1.compareToIgnoreCase(target_agi2);
         }
     }
 
@@ -163,6 +275,5 @@ public class PantherServerWrapper {
         int[] taxonIds = {3702};
 //        ps.readMsaByIdFromServer("PTHR10000");
         ps.readPrunedPantherTreeById("PTHR11913", taxonIds);
-
     }
 }
